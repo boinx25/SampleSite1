a16871f543e74b5b64cb171846dc65fcdd9de99b
Boinx Bantug <boinxbantug25@yahoo.com>
2013-12-03 11:15:50 +0800

    Prev and Next Button now working.

--- trial/slide.js
+++ trial/slide.js
@@ -6,21 +6,55 @@ $(document).ready(function()
 	$("#next").on('click',function()
 	{
 		var currentMargin = ($(".imageContainer").css("margin-left"));
-		console.log(currentMargin);
+		// console.log(currentMargin);
 		currentMargin=currentMargin.replace("px","");
-		console.log(currentMargin);
+		// console.log(currentMargin);
 		var finalMargin = parseInt(currentMargin);
 		var NextMargin = (finalMargin - 275);
-		console.log(NextMargin);
+		// console.log(NextMargin);
 
-		$(".imageContainer").animate({"margin-left":NextMargin +"px"},1000,function()
+		
+		if(NextMargin > -825)
 		{
+
+			$(".imageContainer").animate({"margin-left":NextMargin +"px"},1000,function()
+			{
 			// console.log($(this).css("margin-left"));
-			
 
-
-		});
+			});
+		}
 	});
+
+
+	$("#prev").on('click',function()
+	{
+		var currentMargin = ($(".imageContainer").css("margin-left"));
+		// console.log(currentMargin);
+		currentMargin=currentMargin.replace("px","");
+		// console.log(currentMargin);
+		var finalMargin = parseInt(currentMargin);
+		var NextMarginPrev = (finalMargin + 275);
+		console.log(NextMarginPrev);
+
+		
+		if(NextMarginPrev <=0)
+		{
+
+			$(".imageContainer").animate({"margin-left":NextMarginPrev +"px"},1000,function()
+			{
+			// console.log($(this).css("margin-left"));
+
+			});
+		}
+	});
+
+
+
+	
+
+
+
+
 
 });	
 
