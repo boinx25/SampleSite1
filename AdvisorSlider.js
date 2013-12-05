93ab52788c80dd599c08a2780e3bd239413fb27c
Boinx Bantug <boinxbantug25@yahoo.com>
2013-12-05 10:24:55 +0800

    Mouseleave Mouseover fixed.

--- AdvisorSlider.js
+++ AdvisorSlider.js
@@ -1,99 +1,117 @@
 $(document).ready(function()
 
 {
-	$("#pause").hide(200);
-	var autoplay = null;
-	var isAutoPlay = false;
-	var isHovering= false;
+        $("#pause").hide(200);
+        var autoplay = null;
+        var isAutoPlay = false;
+        var isHovering= false;
 
-	function onNextSlide()
-	{
+        function onNextSlide()
+        {
 
-			$(".Imageslider").animate({"margin-left":"-100%"},1000,function()
-			{
-
-
-				$(".Imageslider img:first-child").appendTo(".Imageslider");
-				$(".Imageslider").css("margin-left", "0px");		
-
-			});
-
-	}
-
-	function onPreviousSlide()
-	{
-			$(".Imageslider img:last-child").prependTo(".Imageslider");
-			$(".Igmageslider").css("margin-left", "-100%");		
-			
-			$(".Imageslider").animate({"margin-left":"0px"},1000);
-	}
-
-	function Pause()
-	{
-			clearInterval(autoplay);
-			$('#play').show(100);
-			$('#pause').hide(300);
-			
-	}
+                        $(".Imageslider").animate({"margin-left":"-100%"},1000,function()
+                        {
 
 
+                                $(".Imageslider img:first-child").appendTo(".Imageslider");
+                                $(".Imageslider").css("margin-left", "0px");                
 
+                        });
 
-	$("#next").click(onNextSlide);
-	
+        }
 
+        function onPreviousSlide()
+        {
+                        $(".Imageslider img:last-child").prependTo(".Imageslider");
+                        $(".Igmageslider").css("margin-left", "-100%");                
+                        
+                        $(".Imageslider").animate({"margin-left":"0px"},1000);
+        }
 
-	$("#prev").click(onPreviousSlide);
+        function Pause()
+        {
+        				if(isAutoPlay == isAutoPlay)
+        				{
+                        clearInterval(autoplay);
+                        $('#play').show(100);
+                        $('#pause').hide(300);
+                    	}else
+                    	{}
+                        
+        }
 
 
 
-			$('#play').on('click',function()
-			{
-				$('#pause').show(100);
-				$('#play').hide(300);
-				
-			clearInterval(autoplay);
-			autoplay = setInterval(function()
-			{
-		
-				$(".Imageslider").animate({"margin-left":"-100%"},1000,function()
-			{
+
+        $("#next").click(onNextSlide);
+        
 
 
-				$(".Imageslider img:first-child").appendTo(".Imageslider");
-				$(".Imageslider").css("margin-left", "0px");		
-
-			});
-
-			},4000);
-			});
-
-	
-			$('#pause').click(Pause);
+        $("#prev").click(onPreviousSlide);
 
 
-			$('.Imageslider').mouseover(function()
-			{
-				clearInterval(autoplay);
-			});
 
-			$('.Imageslider').mouseleave(function()
-			{
-				clearInterval(autoplay);
-				autoplay = setInterval(function()
-				{
+        $('#play').on('click',function()
+             {
+                   $('#pause').show(100);
+                   $('#play').hide(300);
+                   
+                   if(isAutoPlay == isAutoPlay)
+                   {
 
-					$(".Imageslider").animate({"margin-left":"-100%"},1000,function()
-					{
-
-						$(".Imageslider img:first-child").appendTo(".Imageslider");
-						$(".Imageslider").css("margin-left", "0px");		
-
-					});
-
-				},4000);
-			});
-
-});	
+                   
+                      clearInterval(autoplay);
+                      autoplay = setInterval(function()
+                      {
+                
+                            $(".Imageslider").animate({"margin-left":"-100%"},1000,function()
+                        {
 
 
+                            $(".Imageslider img:first-child").appendTo(".Imageslider");
+                            $(".Imageslider").css("margin-left", "0px");                
+
+                        		console.log("abc");
+                        });
+
+                      },4000);
+                    }
+           });
+        
+                        $('#pause').click(Pause);
+
+
+                        $('.Imageslider').mouseover(function()
+                        {
+                                clearInterval(autoplay);
+                                $('#play').show(100);
+                        		$('#pause').hide(200);
+                        });
+
+                        $('#leftImg').mouseleave(function()
+                        {
+                        		$('#pause').show(100);
+                        		$('#play').hide(200);
+                                clearInterval(autoplay);
+                                if(isAutoPlay !== isAutoPlay, isHovering == isHovering )
+                                {
+                                	console.log("hello");
+                                	autoplay = setInterval(function()
+                                	{
+
+                                        $(".Imageslider").animate({"margin-left":"-100%"},1000,function()
+                                        {
+
+                                                $(".Imageslider img:first-child").appendTo(".Imageslider");
+                                                $(".Imageslider").css("margin-left", "0px");                
+
+                                        });
+
+                                	},4000);
+                            	}else
+                            	   {
+                            		console.log("abc");
+                            	   }
+                        });
+
+});        
